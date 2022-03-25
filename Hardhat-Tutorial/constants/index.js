bf087b69451be0f7cfc0fef1939eb20d89704fba
david <dayveed1@users.noreply.github.com>
2022-03-25 13:57:45 +0100

    My first push to MY-NFT-Collection

---
+++ Hardhat-Tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xf1be91c87E040dEA7e7E7f97C954b2C5A486ceb4";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
